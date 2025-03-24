da82784b20c3b256a361b7660618ac48515fd7fb
Haaid Razi Khan <haaidrazikhan11@gmail.com>
2025-03-24 14:38:38 +0500

    Add files via upload

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Chatbot from "@/app/components/chatbot"
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+      <Chatbot />
+    </div>
+  );
+}
